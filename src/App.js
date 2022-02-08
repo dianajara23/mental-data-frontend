97ece4390b6cf82bdb8797af4a1bce6e94763531
dianajara23 <dpjara23@ucatolica.edu.co>
2022-02-08 15:41:18 -0500

    He instalado la librería Chart.Js para React. También he creado   un componente para realizar una gráfica de barras.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import BarChart from "./components/BarChart";
+import "./App.css";
 
 function App() {
-  useEffect( ()=>{
-    fetch('http://localhost:3000/users')
-      .then(response=> {
-        return response.json()
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((response) => {
+        return response.json();
       })
-      .then(data=> {
+      .then((data) => {
         console.log(data);
-      })
-  },[]);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mental Data</h1>
+      <div className="Chart-container">
+        <BarChart></BarChart>
+      </div>
     </div>
   );
 }
